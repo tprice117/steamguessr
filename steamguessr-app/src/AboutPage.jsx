a7e084bcc6cf240ceab26c2fec689d0a09ac45b7
Tyler Price <tylerprice117@gmail.com>
2025-06-05 01:56:06 -0500

    DOM Migration, and recreation

---
+++ steamguessr-app/src/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div style={{ padding: "2em" }}>
+      <h1>About SteamGuessr</h1>
+      <p>This is a guessing game using Steam reviews and game data.</p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ steamguessr-app/src/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div style={{ padding: "2em" }}>
+      <h1>SteamGuessr!</h1>
+      <p>Welcome to the SteamGuessr home page.</p>
+      {/* You can move your main game UI here */}
+    </div>
+  );
+}
+
+export default HomePage;
